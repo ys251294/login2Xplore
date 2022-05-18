419dfa24f2e0c427424c8777c4d232de05486be3
Yogender Singh <ys251294@gmail.com>
2022-05-18 21:30:14 +0530

    Update index.html, main-style.css, reorder-script.js

--- reorder-script.js
+++ reorder-script.js
@@ -1,37 +1,41 @@
-'use strict';
+"use strict";
 
 
 function reorder(sentence) {
-
     let words = sentence.split(" ");
-// console.log(words);
+    // console.log(words);
 
-// for storing numbers present in words.
+    // for storing numbers present in words.
 
-let numbers = [];
+    let numbers = [];
 
-let len = -1;
-while (++len < words.length) {
-    for (let i = 0; i < words[len].length; i++) {
-        let num = Number(words[len].charAt(i));
-        if (num >= 1 && num <= 9) {
-            numbers.push(num);
-            words[len] = words[len].replace(num, '');
+    let len = -1;
+    while (++len < words.length) {
+        for (let i = 0; i < words[len].length; i++) {
+            let num = Number(words[len].charAt(i));
+            if (num >= 1 && num <= 9) {
+                numbers.push(num);
+                words[len] = words[len].replace(num, "");
+            }
+        }
+    }
+    // console.log(numbers, words);
+
+    // Rearranging words to make proper sentences
+    let num = 1;
+    let result = "";
+    for (let i = 0; i < 9; i++) {
+        if (numbers[i] == num) {
+            result += words[i];
+            num++;
+            if (num > numbers.length) break;
+            result += " ";
+            i = -1;
         }
     }
 }
-// console.log(numbers, words);
 
-// Rearranging words to make proper sentences
-let num=1;
-let result="";
-for(let i=0; i<9; i++){
-    if(numbers[i]==num){
-        result += words[i];
-        num++;
-        if(num>numbers.length) break;
-        result += " ";
-        i = -1;
-    }
-}
+function addDataInDB(name, email, mobileno, unorderedSentence, orderedSentence) {
+    const token = "90936035|-31949284084870622|90940478"
+    createPUTRequest(token, jsonObj, dbName, relName);
 }
